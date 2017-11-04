77546bf47e79ccf5347421ef9d3ce0f3abbbfa54
Ositadinma Nwangwu <Ositadinma Nwangwu>
2017-11-04 17:10:17 +0100

    preliminary routes.. just a test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use($router){
+    $router->get('/office_entity_types', function (){
+       $ent = \App\Models\OfficeEntityType::all();
+       return $ent;
+    });
+    $router->post('/office_entity_type/create', function (\Illuminate\Http\Request $request){
+        if($c = \App\Models\OfficeEntityType::create([
+            'name' => $request->name
+        ])){
+            return response()->json(['message' => 'you just created an entity type', $c]);
+        }
+    });
+});
+
+
