fa8d3a4049071d75183e9e266c7aa78611d32a9e
Harrison Favour <favour.harrison@tenece.com>
2018-02-01 08:58:53 +0000

    added date formatter

--- public/app/compiled/js/tenece_vouchers.js
+++ public/app/compiled/js/tenece_vouchers.js
@@ -43653,13 +43653,44 @@ app.config(['$httpProvider', '$interpolateProvider', '$locationProvider', '$stat
     };
 
 
-}]);;app.controller('MainController', ['$scope', '$cookies', '$state', 'MainService', function ($scope, $cookies, $state, MainService) {
+}]);;app.controller('MainController', ['$rootScope', '$scope', '$cookies', '$state', 'MainService', function ($rootScope, $scope, $cookies, $state, MainService) {
 
     var state = $cookies.get('state');
     if(state) {
         $state.go(state);
         $cookies.remove('state');
     }
+
+    $rootScope.formatDate = function (timeStamp) {
+        // return timeStamp;
+        var date = new Date(timeStamp);
+        var day = date.getDay();
+        switch(day) {
+            case 0 :
+                day = 'Sunday';
+                break;
+            case 1 :
+                day = 'Monday';
+                break;
+            case 2 :
+                day = 'Tuesday';
+                break;
+            case 3 : 
+                day = 'Wednesday';
+                break;
+            case 4 :
+                day = 'Thursday';
+                break;
+            case 5 :
+                day = 'Friday';
+                break;
+            case 6 : 
+                day = 'Saturday';
+                break;
+        }
+        return day + ', ' + date.toLocaleString();
+    };
+    
 }]);
 
 app.service('MainService', ['APIService', function (APIService) {
